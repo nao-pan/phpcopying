2f71b76428aaf5babddeb8f9beaf05988d34dfec
nao-pan <n.hasegawa.su@gmail.com>
2025-07-04 11:41:40 +0900

    トップのインデックスを作成、ディレクトリ構成を見直し

--- index.php
+++ index.php
@@ -1,50 +1,53 @@
-<?php
-$filename = 'memos.csv';
-
-//POST情報をcsv書き込み
-if($_SERVER['REQUEST_METHOD']==='POST' && !empty($_POST['memo'])){
-  $memo = trim($_POST['memo']);
-  $datetime = (date('Y-m-d H:i:s'));
-  $fp = fopen($filename, 'a');
-  fputcsv($fp, [$memo, $datetime]);
-  fclose($fp);
-}
-?>
-
-<!-- index.php -->
- <!DOCTYPE <!DOCTYPE html>
- <html lang="ja">
-  <head>
-    <meta charset="utf-8">
-    <title>PHPメモ帳</title>
-    <link rel="stylesheet" href="">
-  </head>
-  <body>
-    <h1>メモ帳アプリ</h1>
-    <form method="POST" action="index.php">
-      <textarea name="memo" rows="5" cols="40" placeholder="ここにメモを記入してください"></textarea><br>
-      <button type="submit">保存</button>
-    </form>
-
-    <?php
-    $memos = [];
-    //csv情報を読み込み
-    if(file_exists($filename)){
-      $fp = fopen($filename, 'r');
-      while(($data = fgetcsv($fp)) !== false){
-        $memos[] = $data;
-      }
-      fclose($fp);
-    };
-    ?>
-    <ul>
-      <?php foreach ($memos as $entry): ?>
-        <li>
-          <?= htmlspecialchars($entry[1], ENT_QUOTES, 'UTF-8') ?>
-          :
-          <?= htmlspecialchars($entry[0], ENT_QUOTES, 'UTF-8') ?>
-      </li>
-      <?php endforeach; ?>
-    </ul>
-  </body>
- </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>PHP アプリケーション 写経の間</title>
+</head>
+<body>
+<section class="text-gray-600 body-font">
+  <div class="container flex flex-wrap px-5 py-24 mx-auto items-center">
+    <div class="md:w-1/2 md:pr-12 md:py-8 md:border-r md:border-b-0 mb-10 md:mb-0 pb-10 border-b border-gray-200">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">PHP アプリケーション</h1>
+      <p class="leading-relaxed text-base">PHPアプリケーションの写経を行います。各リンクからアプリケーションのインデックスに飛ぶようになっています。</p>
+      <a class="text-indigo-500 inline-flex items-center mt-4">Learn More
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+          <path d="M5 12h14M12 5l7 7-7 7"></path>
+        </svg>
+      </a>
+    </div>
+    <div class="flex flex-col md:w-1/2 md:pl-12">
+      <h2 class="title-font font-semibold text-gray-800 tracking-wider text-sm mb-3">CATEGORIES</h2>
+      <nav class="flex flex-wrap list-none -mb-1">
+        <li class="lg:w-1/3 mb-1 w-1/2">
+          <a class="text-gray-600 hover:text-gray-800" href="memo/index.php">メモ帳アプリ</a>
+        </li>
+        <li class="lg:w-1/3 mb-1 w-1/2">
+          <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+        </li>
+        <li class="lg:w-1/3 mb-1 w-1/2">
+          <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+        </li>
+        <li class="lg:w-1/3 mb-1 w-1/2">
+          <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+        </li>
+        <li class="lg:w-1/3 mb-1 w-1/2">
+          <a class="text-gray-600 hover:text-gray-800">Fifth Link</a>
+        </li>
+        <li class="lg:w-1/3 mb-1 w-1/2">
+          <a class="text-gray-600 hover:text-gray-800">Sixth Link</a>
+        </li>
+        <li class="lg:w-1/3 mb-1 w-1/2">
+          <a class="text-gray-600 hover:text-gray-800">Seventh Link</a>
+        </li>
+        <li class="lg:w-1/3 mb-1 w-1/2">
+          <a class="text-gray-600 hover:text-gray-800">Eighth Link</a>
+        </li>
+      </nav>
+    </div>
+  </div>
+</section>
+</body>
+</html>
